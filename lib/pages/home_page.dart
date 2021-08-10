7aac088d2bcb8458e1c401a52decdb6dad8f5ed0
Dew Parin <parinsura@gmail.com>
2021-08-10 11:46:24 +0700

    Set page indicator.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,26 +3,37 @@ import 'package:carb_poc/components/my_goals/my_goals_data.dart';
 import 'package:carb_poc/components/weekly_bar_chart.dart';
 import 'package:carb_poc/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:page_indicator/page_indicator.dart';
 
 const mockCarbs = [10, 8, 14, 9, 8, 11, 18];
 
 class HomePage extends StatelessWidget {
+  final PageController pageController = PageController(initialPage: 0);
+
   @override
   Widget build(BuildContext context) {
-    final PageController pageController = PageController(initialPage: 0);
     return Scaffold(
       body: SafeArea(
-        child: PageView(
-          controller: pageController,
-          scrollDirection: Axis.horizontal,
-          children: [
-            _buildMyWeekChart(),
-            _buildMyGoals(),
-          ],
-        ),
+        child: _buildPageView(),
       ),
     );
   }
+
+  Widget _buildPageView() => PageIndicatorContainer(
+      child: PageView(
+        children: [
+          _buildMyWeekChart(),
+          _buildMyGoals(),
+        ],
+        controller: pageController,
+      ),
+      align: IndicatorAlign.bottom,
+      length: 2,
+      indicatorSpace: 20.0,
+      padding: const EdgeInsets.all(10),
+      indicatorColor: Colors.black12,
+      indicatorSelectorColor: Colors.blue,
+      shape: IndicatorShape.circle(size: 12));
 
   Widget _buildMyWeekChart() => Container(
         padding: EdgeInsets.all(defaultPadding),
